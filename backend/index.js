75441078fa15e3746e6099b50fd8fc60774e8a44
Noor Abdullah <sheikhnoorabdullah02@gmail.com>
2026-01-03 13:10:24 +0600

    Add

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+    return res.json({
+        message: "App is On"
+    })
+})
+
 app.use(userRoutes)
 
 
