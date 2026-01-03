120d3050e425b8c1ca567e0fef3d5b4d4773f5b9
Noor Abdullah <sheikhnoorabdullah02@gmail.com>
2026-01-03 14:54:26 +0600

    Add

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,13 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(cors({
+    origin: 'https://noor-abdullah.vercel.app',
+    credentials: true
+}));
+
+
 app.use(express.json());
-
-app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
